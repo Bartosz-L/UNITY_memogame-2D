17b8072b0903da9adf2e4af1525293b19e15c66b
Your Name <you@example.com>
2019-01-08 16:57:29 +0100

    complete project

---
+++ Assets/scripts/Board.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Board : MonoBehaviour {
+
+    public GameObject TilePrefab;
+
+    public Sprite[] sprites;
+
+    private Tile[] Tiles;
+
+    public Vector2 TilesOffset = Vector2.one;
+
+    public int Width = 6;
+    public int Height = 4;
+
+    public bool CanMove = false;
+
+    public TextMesh WinText;
+
+
+	// Use this for initialization
+	IEnumerator Start () {
+
+        WinText.GetComponent<Renderer>().enabled = false;
+        // generate tiles
+        CreateTiles();
+        // shuffle tiles before placing them
+        ShuffleTiles();
+        // place tiles
+        PlaceTiles();
+
+        CanMove = false;
+        yield return new WaitForSeconds(2f);
+        HideTiles();
+        CanMove = true;
+    }
+
+    void CreateTiles()
+    {
+        // loop for generating board (size: Width by Height)
+        var length = Width * Height;
+        Tiles = new Tile[length];
+
+        for(int i=0; i<length; i++)
+        {
+            var sprite = sprites[i / 2];
+            Tiles[i] = CreateSingleTile(sprite);
+        }
+    }
+
+    Tile CreateSingleTile(Sprite faceSprite)
+    {
+        // create single tile
+        var gameObject = Instantiate(TilePrefab);
+
+        // give tiles new parent (transform from Board obj)
+        gameObject.transform.parent = transform;
+        // change front face of the tile
+        var tile = gameObject.GetComponent<Tile>();
+        tile.frontFace = faceSprite;
+        tile.Uncovered = true;
+
+        return tile;
+    }
+    void ShuffleTiles()
+    {
+        // shuffle x times for better randomization
+        for(int i=0; i<100; i++)
+        {
+            // random index
+            int index1 = Random.Range(0, Tiles.Length);
+            int index2 = Random.Range(0, Tiles.Length);
+            // reference to object
+            var tile1 = Tiles[index1];
+            var tile2 = Tiles[index2];
+            // change place of tiles
+            Tiles[index1] = tile2;
+            Tiles[index2] = tile1;
+        }
+    }
+
+    void PlaceTiles()
+    {
+        for (int i = 0; i < Width * Height; i++)
+        {
+            int x = i % Width;
+            int y = i / Width;
+
+            Tiles[i].transform.localPosition = new Vector3(x * TilesOffset.x, y * TilesOffset.y, 0);
+
+        }
+    }
+
+    void HideTiles()
+    {
+        Tiles.ToList().ForEach(tile => tile.Uncovered = false);
+    }
+
+    private bool CheckIfEnd()
+        // check if all tiles are inactive
+    {
+        return Tiles.All(tile => !tile.Active);
+    }
+
+    public void CheckPair()
+    {
+        StartCoroutine(CheckPairCorouitine());
+    }
+	
+    private IEnumerator CheckPairCorouitine()
+    {
+        var tilesUncovered = Tiles.Where(tile => tile.Active).Where(tile => tile.Uncovered).ToArray();
+
+        if (tilesUncovered.Length != 2) yield break;
+
+        var tile1 = tilesUncovered[0];
+        var tile2 = tilesUncovered[1];
+
+        CanMove = false;
+        yield return new WaitForSeconds(1f);
+        CanMove = true;
+
+        if (tile1.frontFace == tile2.frontFace)
+        {
+            tile1.Active = false;
+            tile2.Active = false;
+        } 
+        else
+        {
+            tile1.Uncovered = false;
+            tile2.Uncovered = false;
+        }
+
+        // end of the game
+        if (CheckIfEnd())
+        {
+            CanMove = false;
+            Debug.Log("The end.");
+            WinText.GetComponent<Renderer>().enabled = true;
+
+
+            yield return new WaitForSeconds(3f);
+            Application.Quit();
+        }
+    }
+
+}
---
+++ Assets/scripts/Tile.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour {
+
+    // state when tile is active (you can see its front)
+    public bool Uncovered = true;
+    // tile is currently on board
+    public bool Active = true;
+    // front texture of the tile
+    public Sprite frontFace;
+
+
+	// Use this for initialization
+	void Start () {
+        // initialize rotation state
+        transform.rotation = GetTargetRotation();
+
+        //search for front object in tile
+        var frontObject = transform.Find("front");
+        var spriteRenderer = frontObject.transform.GetComponent<SpriteRenderer>();
+        // change sprite to frontFace obj
+        spriteRenderer.sprite = frontFace;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // get current tile location
+        var targetRotation = GetTargetRotation();
+        // transform rotation using Lerp() - three parameters: - 1. start state = transform.rotation 2. finish state = targetRotation 3. animation duration
+        transform.rotation = Quaternion.Lerp(
+                transform.rotation,
+                targetRotation,
+                //to speed up animation multiply by 5
+                Time.deltaTime * 5f
+            );
+
+        if (Active == false)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+
+    //quaternion function for rotating tile
+    Quaternion GetTargetRotation()
+    {
+        // if tile is active vector is zero, if not active rotate it by 180 deg
+        var rotation = Uncovered ? Vector3.zero : (Vector3.up * 180f);
+        return Quaternion.Euler(rotation);
+    }
+
+    private void OnMouseDown()
+    {
+        var board = FindObjectOfType<Board>();
+
+        if (board.CanMove == false) return;
+        Uncovered = !Uncovered;
+        board.CheckPair();
+    }
+}
